4c51151939dfcf25b6f94a9a2985ed2d3a1f4996
Guilherme Araújo <guilhermguilhermegui888@gmail.com>
2026-02-06 08:17:58 -0300

    codigo sujo

---
+++ langextract/validacao.py
@@ -0,0 +1,7 @@
+import os
+
+lista_quebardaaaa=[3112,12,12,897,21,212,1,212,34254254,3,5234,4,311,3434,4134,3413414,14,14314,13413,41414,14,13413,41,21221]
+
+acao= input("Digite a acao desejada")
+
+eval(acao)
